a22c04c223d2ca7c8217ef8acdd2629cfe5e15bf
Viachaslau_Koshman <koshman.vyachaslau@gmail.com>
2019-03-15 12:00:42 +0300

    Issue is corrected

--- src/parser/CarsFileReader.java
+++ src/parser/CarsFileReader.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CarsFileReader {
@@ -13,7 +13,7 @@ public class CarsFileReader {
         try {
             return Files.readAllLines(Paths.get(filePath));
         } catch (IOException e) {
-            return new ArrayList<>();
+            return Collections.emptyList();
         }
     }
 }
