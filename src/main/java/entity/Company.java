041cefcec62b4a502bf2aac2b0c702ed7b2a0de6
Slava Koshman <koshman_vyachesl@mail.ru>
2019-03-17 22:09:51 +0300

    Issues are corrected

--- src/main/java/entity/Company.java
+++ src/main/java/entity/Company.java
@@ -58,11 +58,9 @@ public class Company {
             final Car car = carIterator.next();
 
             if (!customer.isTripDone()) {
-
                 if (!carIterator.hasNext()) {
                     LOGGER.info("No free cars, wait");
                 }
-
                 LOGGER.info("Car with ID " + carIterator.next().getId() + " is found for client with ID " + customer.getId());
                 car.occupy(customer);
                 carIterator.remove();
@@ -76,7 +74,7 @@ public class Company {
 
                 car.release();
                 cars.add(car);
-                LOGGER.info(cars.size() + " cars are available:");
+                LOGGER.info(cars.size() + " cars are available");
             }
         }
     }
@@ -86,7 +84,7 @@ public class Company {
     }
 
     private void showCompanyParkInfo() {
-        LOGGER.info(cars.size() + " cars are available:");
+        LOGGER.info(cars.size() + " cars are available");
 
         for (Car car : cars) {
             LOGGER.info("Car with ID " + car.getId());
