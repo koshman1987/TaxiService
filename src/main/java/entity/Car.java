e141d3c3a7c10e2bbf50af26f28985fb78c5d8fb
Viachaslau_Koshman <koshman.vyachaslau@gmail.com>
2019-03-19 10:03:20 +0300

    Refactoring

--- src/main/java/entity/Car.java
+++ src/main/java/entity/Car.java
@@ -3,8 +3,14 @@ package entity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Car {
     private int id;
+    private Lock lock = new ReentrantLock();
+    private AtomicBoolean isFree = new AtomicBoolean(true);
     private static final Logger LOGGER = LogManager.getLogger(Car.class);
 
     public Car(int id) {
@@ -16,12 +22,31 @@ public class Car {
     }
 
     public void occupy(final Customer customer) {
-        LOGGER.info("Order for client with ID " + customer.getId() + " is being executed");
-        customer.setTripIsDone(true);
+        lock.lock();
+
+        try {
+            LOGGER.info("Order for client with ID " + customer.getId() + " is being executed");
+            isFree.set(false);
+        } finally {
+            lock.unlock();
+        }
+
     }
 
-    public void release() {
-        LOGGER.info("Order is completed. Car with ID " + getId() + "  returned to the parking lot");
+    public void release(final Customer customer) {
+        lock.lock();
+
+        try {
+            LOGGER.info("Order is completed. Car with ID " + getId() + "  returned to the parking lot");
+            isFree.set(true);
+            customer.setTripIsDone(true);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public AtomicBoolean getState() {
+        return isFree;
     }
 
     @Override
